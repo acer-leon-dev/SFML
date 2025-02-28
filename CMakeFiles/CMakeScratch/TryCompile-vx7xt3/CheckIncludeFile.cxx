c6b45a7ec370a024215d221250ad367329786a43
leon-acer <leonv.allotey@gmail.com>
2025-02-28 07:52:42 -0600

    setup premake script

---
+++ CMakeFiles/CMakeScratch/TryCompile-vx7xt3/CheckIncludeFile.cxx
@@ -0,0 +1,6 @@
+#include <dinput.h>
+
+int main()
+{
+  return 0;
+}
---
+++ bin-int/Debug-windows-/SFML-Graphics/BlendMode.d
@@ -0,0 +1,8 @@
+bin-int/Debug-windows-/SFML-Graphics/BlendMode.o: \
+ src/SFML/Graphics/BlendMode.cpp \
+ C:/msys64/ucrt64/include/SFML/Graphics/BlendMode.hpp \
+ C:/msys64/ucrt64/include/SFML/Graphics/Export.hpp \
+ C:/msys64/ucrt64/include/SFML/Config.hpp
+C:/msys64/ucrt64/include/SFML/Graphics/BlendMode.hpp:
+C:/msys64/ucrt64/include/SFML/Graphics/Export.hpp:
+C:/msys64/ucrt64/include/SFML/Config.hpp:
